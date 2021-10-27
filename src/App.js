14a9fa0ea450f498ce6934aa1ae9b9675b88c6fe
Hextech43 <oluwatoyine86@gmail.com>
2021-10-27 14:24:18 +0100

    esther work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {BrowserRouter, Switch, Route} from "react-router-dom" 
+import NewUser from "./Esther/UserSign"
+import Company from "./Fedrick/Company"
+import Admin from "./Fedrick/AdminSignin"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <BrowserRouter>
+   <Switch>
+     <Route exact path="/User" component={NewUser}/>
+     <Route exact path="/companyRegister" component={Company}/>
+     <Route exact path="/admin" component={Admin}/>
+   </Switch>
+   </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
