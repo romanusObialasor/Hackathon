f0c7aa1acacc52859ea7fde154520d0b1d2065fe
Hextech43 <oluwatoyine86@gmail.com>
2021-10-27 14:33:18 +0100

    new change

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import {BrowserRouter, Switch, Route} from "react-router-dom" 
-import NewUser from "./Esther/UserSign"
-import Company from "./Fedrick/Company"
-import Admin from "./Fedrick/AdminSignin"
+// import React from 'react'
+// import {BrowserRouter, Switch, Route} from "react-router-dom" 
+// import NewUser from "./Esther/UserSign"
+// import Company from "./Fedrick/Company"
+// import Admin from "./Fedrick/AdminSignin"
 
-const App = () => {
-  return (
-   <BrowserRouter>
-   <Switch>
-     <Route exact path="/User" component={NewUser}/>
-     <Route exact path="/companyRegister" component={Company}/>
-     <Route exact path="/admin" component={Admin}/>
-   </Switch>
-   </BrowserRouter>
-  )
-}
+// const App = () => {
+//   return (
+//    <BrowserRouter>
+//    <Switch>
+//      <Route exact path="/User" component={NewUser}/>
+//      <Route exact path="/companyRegister" component={Company}/>
+//      <Route exact path="/admin" component={Admin}/>
+//    </Switch>
+//    </BrowserRouter>
+//   )
+// }
 
-export default App
+// export default App
